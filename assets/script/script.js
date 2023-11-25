8e31e718340c9c581647daf3d7864cd586279ff6
crypticsurfer <cb.connor.bishop@gmail.com>
2023-11-24 22:25:35 -0700

    added querey and api and started js

---
+++ assets/script/script.js
@@ -0,0 +1,6 @@
+let apiKey = "428568c242a23aec5b8d74156afd47b7"
+let queryUrl = "https://api.openweathermap.org/data/2.5/weather?"
+
+
+let searchButton = document.querySelector("#search")
+let searchEl = document.querySelector("#searchfield")
